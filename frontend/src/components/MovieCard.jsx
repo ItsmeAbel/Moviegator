41e55c2ae7353e6ef68f6bc70159e2246b6441e8
It's me Abel <abel97.ag@gmail.com>
2025-07-16 18:55:03 +0200

    MovieCardComponent
    Added the MovieCard component used to display each movie

---
+++ frontend/src/components/MovieCard.jsx
@@ -0,0 +1,27 @@
+function MovieCard({Movie}){
+
+    function onFavoriteClick() {
+
+        alert('You clicked on the favorite button');
+
+    }
+
+    return (
+        <div className="movie-card">
+            <div className="movie-poster">
+                <img src={Movie.url} alt={Movie.title} />
+                <div className="movie-overlay">
+                    <button className = "favorite-button" onClick={onFavoriteClick}>Like</button>
+
+                </div>
+            </div>
+            <div className="movie-info">
+                <h1>{Movie.title}</h1>
+                <p>{Movie.release_date}</p>
+            </div>
+        </div>
+        
+    );
+}
+
+export default MovieCard;
