b66ac954d295d1fd2b5483bb783ecf172c4e54a4
It's me Abel <abel97.ag@gmail.com>
2025-07-20 20:43:06 +0200

    fixed some wrappers
    faulty wrapping caused some routing issues in the favorites tab

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,23 +2,27 @@ import "./css/App.css";
 import MovieCard from "./components/MovieCard";
 import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { MovieProvider } from "./contexts/MovieContext";
 
 function App() {
   return (
-    <MovieProvider>
-    <div>
-      <Navbar/>
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
-    </main>
-    </div>
-    </MovieProvider>
+    <React.StrictMode>
+      <BrowserRouter>
+        <MovieProvider>
+          <div>
+            <Navbar />
+            <main className="main-content">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/favorites" element={<Favorites />} />
+              </Routes>
+            </main>
+          </div>
+        </MovieProvider>
+      </BrowserRouter>
+    </React.StrictMode>
   );
 }
 
