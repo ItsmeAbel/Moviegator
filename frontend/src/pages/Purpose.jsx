ace9fc8594578a03d5040a5326724001b887b201
It's me Abel <abel97.ag@gmail.com>
2025-07-23 21:29:53 +0200

    updated purpose page
    updated the purpose page
    installed firebase package
    ---upcoming:
    user login to save favorites

--- frontend/src/pages/Purpose.jsx
+++ frontend/src/pages/Purpose.jsx
@@ -8,8 +8,9 @@ function Purpose(){
 
         <div>
             <h1> Why use Moviegator?</h1>
-            <p>Simply put, Moviegator helps you keep track of the movies you've watched.</p>
+            <p>Simply put, Moviegator helps you keep track of some of your favorite movies that you've watched.</p>
             <p>You can add the movies you've watched as favorites, and they will be stored locally in the browser and can be found in the favorites page.</p>
+            <p>The currently most popular movies are displayed on the front page of the website where you can also see their public rating and have the option to watch their trailers.</p>
             <p>For any inquiries contact me at <a href="mailto:abel97.ag@gmail.com?subject=Hello%20Abel">abel97.ag@gmail.com</a></p>
             <p></p>
 
